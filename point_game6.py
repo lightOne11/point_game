c7ae05ee8e5e56b035fef5e87df5059d6d16f906
lightOne <qqqwww82000@naver.com>
2023-03-10 08:07:56 +0900

    VER.6

---
+++ point_game6.py
@@ -0,0 +1,190 @@
+# 점수 게임
+# 1. 인원 2 (옵션 ~4)
+# 2. 역순으로 처음지점 선택
+# 3. 처음지점에서 좌,우,상,하 이동 (옵션 대각이동)
+# 4. 선택한 곳의 점수를 더함
+# 5. 다음에 갈곳이 없으면 게임 종료
+# 6. 종료 시 점수가 높은쪽이 승
+# 7. 게임판 기본 5*5 (옵션 n)
+# 8. 게임판에는 1~칸수까지 번호가 적혀있음
+# 9. 다른 사람이 선택한 곳은 지나갈 수 없음
+import numpy as np
+import random as rm
+import pandas as pd
+
+class com:
+    def __init__(self,input_X,result_Y,outcome_Y):
+        pass
+    
+
+
+
+
+
+class point_game():
+    def __init__(self,place_n=5,player_n=2,siege=True):
+        self.place_n = place_n
+        self.player_n = player_n
+        self.siege = siege
+        self.player = {}
+        print('-- 참가 플레이어 --')
+        for i in range(1,player_n+1):
+            self.player[f'{i} player'] = []
+            print(f'{i} player')
+        pla = np.array(range(1, place_n*place_n+1))
+        rm.shuffle(pla)
+        self.place = pla.reshape(place_n,place_n).tolist()
+        self.place_o = self.place
+        way_list = self.matinit(place_n)
+        self.main(way_list)
+        self.dir_x1,self.dir_y1 = 0,0
+
+    def matinit(self,place_n):
+        way_list = np.ones((place_n+2,place_n+2),dtype='object')
+        way_list[1:place_n+1,1:place_n+1] = 0
+        return way_list
+        
+
+    def start(self,way,way_list):
+        for i in sorted(range(1,self.player_n+1),reverse=True):
+                    self.display()
+                    while True:
+                        try:
+                            x,y=map(int,input(f'[{i}player] 좌표를 입력하세요').split())
+                            if self.condition(way,way_list,x,y)== True:                                
+                                break
+                        except: pass               
+                    self.player[f'{i} player'].append(int(self.place[x-1][y-1]))
+                    self.player[f'{i} player'].append([[x,y]])
+                    way_list[x,y] = str(i)+'P'
+                    self.place[x-1][y-1] = str(i)+'P'
+                    #print(f'위치 선택 후 위치 점수 : {self.place[x][y]}\n플레이어번호 : {i}')
+
+    def move(self,way,way_list,qi):
+        self.turn += 1
+        print(f'-----{self.turn} round ------')
+        for i in range(1,self.player_n+1):
+            if way == 'x' and i == qi: continue
+            else :
+                # if self.turn > 2 :
+                #     if self.player[f'{i} player'][1][-1] - self.player[f'{i} player'][1][-2] == [-1,0]: 
+                #     elif way == '4' : self.place[x-1][y-1] = '<'
+                #     elif way == '6' : self.place[x-1][y-1] = '>'
+                #     elif way == '2' : self.place[x-1][y-1] = 'v'
+                while True:
+                    self.display()
+                    x,y = self.player[f'{i} player'][1][-1]
+                    way=input(f'[{i} player] 방향을 입력하세요(종료x)')
+                    #print(f'방향 입력 후 입력 : {way}\n입력리스트 \n{way_list}\n현재위치 : {x},{y}\n이동가능여부 : {self.condition(way,way_list,x,y)}')
+                    if self.condition(way,way_list,x,y) == True:
+                        if way == '8': 
+                            self.player[f'{i} player'][1].append([x-1,y])
+                            break
+                        elif way == '4': 
+                            self.player[f'{i} player'][1].append([x,y-1])
+                            break
+                        elif way == '6': 
+                            self.player[f'{i} player'][1].append([x,y+1])
+                            break
+                        elif way == '2': 
+                            self.player[f'{i} player'][1].append([x+1,y])
+                            break
+                    if way == 'x': break
+                    print('방향을 다시 입력해주세요')
+                if way == 'x': break
+                x,y = self.player[f'{i} player'][1][-1]
+                self.player[f'{i} player'][0] +=int(self.place[x-1][y-1])
+                self.place[x-1][y-1] = str(i)+'P'
+                way_list[x,y] = str(i)+'P'
+                for p in range(1,self.player_n+1):
+                    way_p = []
+                    x,y = self.player[f'{p} player'][1][-1]
+                    for l,m in [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]:
+                        way_p.append(way_list[l,m] != 0)
+                        #print(f'이동후 방향 : {l},{m}\n이동 가능 여부 : {way_list[l,m]}\n이동리스트 : \n{way_list}')
+                    #print(f'이동불가방향 : {way_p}')
+                    if way_p == [True,True,True,True] :
+                        way_p = True
+                        break
+                if way_p == True: break   
+            if way == 'x': break
+            if way_p == True: break              
+        self.i = i
+
+    def main(self,way_list):
+        way = ''
+        way_p = []
+        self.i = 1
+        while True: 
+            if len(self.player[f'1 player'])==0:
+                self.start(way,way_list)
+            else :
+                self.turn = 0
+                while True:
+                    self.move(way,way_list,self.i)
+                    if way_p == True: break
+                    if way == 'x': break
+            if way_p == True: break
+            if way == 'x': break
+        if self.siege == True : self.move(way,way_list,self.i)
+        self.exit(self.turn,way,self.i)
+        self.gamesave()
+
+    def gamesave(self):
+        print(self.place_o)
+        print(self.player)
+        #pd.DataFrame(self.place_o+list(self.player)).to_csv('https://drive.google.com/drive/folders/17OdUEiltOKPB4v65Fk5euoJSNOJRmEyr?usp=share_link')
+
+    def exit(self,turn,way,i):
+        self.display()
+        print(f'{turn} round로 게임이 끝났습니다.')
+        if way == 'x' :
+            self.player[f'{i} player'][0] = 0 
+            print(f'{i}player 의 기권입니다.')
+        if len(self.player[f'1 player'])!=0 :
+            win_p = 0
+            for i in range(1,self.player_n+1):
+                p = self.player[f'{i} player']
+                print(f'{i}P :',p[0],'점',end='\t')
+                if win_p < p[0]:
+                    win_p = p[0]
+                    win_n = i
+
+        print(f'\n승자는 {win_n} player 입니다.')
+
+
+    def display(self):
+        for x in range(self.place_n):
+            for y in range(self.place_n):
+                print(f'{str(self.place[x][y]):3s}',end=' ')
+            print()
+        print('------------------')
+        if len(self.player[f'1 player'])!=0 :
+            for i in range(1,self.player_n+1):
+                p = self.player[f'{i} player']
+                print(f'{i}P :',p[0],'점 ,',f'[{p[1][-1][0],p[1][-1][1]}]',end='\t')
+                print()
+
+    def condition(self,way,way_list,x,y):
+        if way == '8' and way_list[x-1,y] != 0:
+            print('(8) 갈수 없는 곳입니다.')
+            return False
+        elif way == '4' and way_list[x,y-1] != 0:
+            print('(4) 갈수 없는 곳입니다.')
+            return False
+        elif way == '6' and way_list[x,y+1] != 0:
+            print('(6) 갈수 없는 곳입니다.')
+            return False
+        elif way == '2' and way_list[x+1,y] != 0:
+            print('(2) 갈수 없는 곳입니다.')
+            return False   
+        elif len(self.player[f'1 player'])==0 and way_list[x,y] != 0:
+            print('(5) 갈수 없는 곳입니다.')
+            return False                    
+        else : 
+            #print(f'현재위치 : {x},{y}')
+            return True
+
+
+
+point_game(7)
